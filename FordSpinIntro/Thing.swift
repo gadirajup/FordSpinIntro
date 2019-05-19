8fa28d727e516e75c9dc3217dd8c36e18535fb3e
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-05-19 09:01:36 -0700

    - collection view added

---
+++ FordSpinIntro/Thing.swift
@@ -0,0 +1,15 @@
+//
+//  Thing.swift
+//  FordSpinIntro
+//
+//  Created by Prudhvi Gadiraju on 5/19/19.
+//  Copyright © 2019 Prudhvi Gadiraju. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Thing {
+    var color: UIColor
+    var height: Int
+}
--- FordSpinIntro/ViewController.swift
+++ FordSpinIntro/ViewController.swift
@@ -8,11 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
+    var things = [Thing]()
+    
+    lazy var collectionView: UICollectionView = {
+        let cv = UICollectionView(frame: self.view.frame, collectionViewLayout: UICollectionViewFlowLayout())
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.delegate = self
+        cv.dataSource = self
+        cv.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        cv.backgroundColor = .white
+        cv.contentInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
+        return cv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+    
+        setupThings()
+        setupCollectionView()
+    }
+    
+    fileprivate func setupCollectionView() {
+        view.addSubview(collectionView)
+        collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 0).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: 0).isActive = true
+    }
+    
+    fileprivate func setupThings() {
+        things.append(Thing(color: .blue, height: 50))
+        things.append(Thing(color: .red, height: 100))
+        things.append((Thing(color: .green, height: 150)))
     }
 }
 
+extension ViewController {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return things.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .cyan
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width = (self.view.frame.width - 16 - 16 - 16) / 2
+        
+        return .init(width: width, height: CGFloat(things[indexPath.item].height))
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 16
+    }
+}
